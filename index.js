525526a5ceb966d2af2448731944efe7b648ac17
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-19 15:35:50 +0300

    completing assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData (userName, userEmail) {
+    const user ={
+        "name": userName,
+        "email": userEmail
+    }
+    const userdata = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(user),
+      };
+
+    return fetch("http://localhost:3000/users", userdata)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        console.log(object);
+        document.body.innerHTML = object.id
+      })
+      .catch(function (error) {
+        console.log(error.message);
+        document.body.innerHTML = error.message
+      })
+}
